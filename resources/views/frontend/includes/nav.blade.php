d5c06c5b4b2d12a98562c3da1bb0cc727c32fdc2
ramaalfin <ramaalfin7@gmail.com>
2023-05-05 11:02:42 +0700

    edit index frontend

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -4,19 +4,8 @@
             <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
         </a>
-        <div class="flex lg:order-2">
-            {{-- <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
-                aria-expanded="false"
-                class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
-                <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                        clip-rule="evenodd"></path>
-                </svg>
-                <span class="sr-only">Search</span>
-            </button> --}}
-            <div class="relative hidden lg:block">
+        <div class="flex lg:order-1">
+            {{-- <div class="relative hidden lg:block">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg">
@@ -29,7 +18,7 @@
                 <input type="text" id="search-navbar"
                     class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Search...">
-            </div>
+            </div> --}}
             <button data-collapse-toggle="navbar-search" type="button"
                 class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="navbar-search" aria-expanded="false">
@@ -43,7 +32,7 @@
             </button>
         </div>
         <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="navbar-search">
-            <div class="relative mt-3 lg:hidden">
+            {{-- <div class="relative mt-3 lg:hidden">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg">
@@ -55,29 +44,29 @@
                 <input type="text" id="search-navbar"
                     class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Search...">
-            </div>
+            </div> --}}
             <ul
                 class="flex flex-col p-4 lg:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-white">
                 <li>
                     <a href="{{ route('frontend.home') }}"
-                        class="block py-2 pl-3 pr-4 {{ request()->is('/') ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100' }} rounded lg:bg-transparent lg:text-blue-700 lg:p-0"
+                        class="block py-2 pl-3 pr-4 {{ request()->is('/') ? 'text-white bg-blue-700 lg:text-blue-700' : 'text-gray-900 hover:bg-gray-100' }} rounded lg:bg-transparent lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0"
                         aria-current="page">Home</a>
                 </li>
                 <li>
                     <a href="{{ route('frontend.allPosts') }}"
-                        class="block py-2 pl-3 pr-4 {{ request()->is('all-posts') ? 'text-white bg-blue-700' : 'text-gray-900 hover:bg-gray-100' }} rounded lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0">All Posts</a>
+                        class="block py-2 pl-3 pr-4 {{ request()->is('all-posts') ? 'text-white bg-blue-700 lg:text-blue-700' : 'text-gray-900 hover:bg-gray-100' }} rounded lg:bg-transparent lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0">All Posts</a>
                 </li>
                 <li>
                     <a href="#"
-                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500">Categories</a>
+                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0">Categories</a>
                 </li>
                 <li>
                     <a href="#"
-                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500">Contact</a>
+                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0">Contact</a>
                 </li>
                 @if (Auth::check())
                     <li class="d-flex">
-                        <a class="py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500" href="{{ route('logout') }}"
+                        <a class="py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
                             <i class="bx bx-power-off "></i>
